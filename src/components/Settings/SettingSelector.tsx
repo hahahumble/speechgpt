f897b02f680565efb1b8b8e308399c88fd565a05
hahahumble <hexichn@gmail.com>
2023-04-04 16:08:04 +0800

    fix: setting selector selected style

--- src/components/Settings/SettingSelector.tsx
+++ src/components/Settings/SettingSelector.tsx
@@ -7,12 +7,12 @@ interface SettingSelectorProps {
 
 function SettingSelector({ selected, onSelect, catalogItems, catalogIcons }: SettingSelectorProps) {
   return (
-    <div className="w-52 px-2">
+    <div className="w-auto px-2">
       {catalogItems.map((item, key) => (
         <div
           key={item}
-          className={`py-1 rounded-lg hover:bg-gray-200 text-gray-700 hover:text-black cursor-pointer text-left pl-4 flex flex-row space-x-2 items-center ${
-            item === selected ? 'selected bg-gray-300 font-medium' : ''
+          className={`font-medium py-1 rounded-lg hover:bg-gray-200 text-gray-600 hover:text-black cursor-pointer text-left pl-4 pr-6 flex flex-row space-x-2 items-center ${
+            item === selected ? 'text-gray-800 selected bg-gray-300 bg-opacity-75 hover:bg-gray-300 hover:bg-opacity-75' : ''
           }`}
           onClick={() => onSelect(item)}
         >
