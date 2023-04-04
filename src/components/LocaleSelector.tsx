8a347d814d16a0a5aa59d69a9d580fab5d562073
hahahumble <hexichn@gmail.com>
2023-04-04 16:07:41 +0800

    fix: locale selector responsive width

--- src/components/LocaleSelector.tsx
+++ src/components/LocaleSelector.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useRef} from 'react';
 import { useTranslation } from 'react-i18next';
 import 'tippy.js/dist/tippy.css';
 import { useGlobalStore } from '../store/module';
@@ -11,9 +11,11 @@ interface LanguageOption {
 function LanguageSelector() {
   const { i18n } = useTranslation();
   const { locale, setLocale } = useGlobalStore();
+  const selectRef = useRef<HTMLSelectElement>(null);
+  const hiddenSpanRef = useRef<HTMLSpanElement>(null);
 
   useEffect(() => {
-    if (locale == '') {
+    if (locale === '') {
       const browserLanguage = navigator.language.split(/[-_]/)[0];
       setLocale(browserLanguage);
     }
@@ -28,22 +30,37 @@ function LanguageSelector() {
     { code: 'zh', name: '中文' },
   ];
 
+  useEffect(() => {
+    if (selectRef.current && hiddenSpanRef.current) {
+      const width = hiddenSpanRef.current.offsetWidth + 25;
+      selectRef.current.style.width = width + 'px';
+    }
+  }, [locale]);
+
   const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setLocale(e.target.value);
   };
 
+  const selectedLanguage = languages.find(lang => lang.code === locale);
+
   return (
-    <select
-      onChange={e => handleLanguageChange(e)}
-      value={locale}
-      className="mr-0.5 w-24 bg-gray-100 text-gray-700 border border-gray-300 rounded-md py-2 px-3 focus:outline-none hover:bg-gray-200 transition duration-100 ease-in-out"
-    >
-      {languages.map(language => (
-        <option key={language.code} value={language.code}>
-          {language.name}
-        </option>
-      ))}
-    </select>
+    <div className="relative inline-block">
+      <span ref={hiddenSpanRef} className="absolute top-0 left-0 opacity-0 -z-10 pr-6">
+        {selectedLanguage?.name}
+      </span>
+      <select
+        ref={selectRef}
+        onChange={e => handleLanguageChange(e)}
+        value={locale}
+        className="mr-0.5 w-auto bg-gray-100 text-gray-700 border border-gray-300 rounded-md py-2 px-3 focus:outline-none hover:bg-gray-200 transition duration-100 ease-in-out"
+      >
+        {languages.map(language => (
+          <option key={language.code} value={language.code}>
+            {language.name}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 }
 
