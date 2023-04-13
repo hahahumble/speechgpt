33097705e663158b96a6b1e8152fbe7d532627bb
hahahumble <hexichn@gmail.com>
2023-04-14 00:58:28 +0800

    style: format code

--- src/components/ConversationPanel.tsx
+++ src/components/ConversationPanel.tsx
@@ -25,11 +25,11 @@ function ConversationPanel({
   const { i18n } = useTranslation();
   const [isHidden, setIsHidden] = useState(false);
   const handleMouseUp = () => {
-    setIsHidden(window.getSelection()?.toString().length !== 0)
-  }
+    setIsHidden(window.getSelection()?.toString().length !== 0);
+  };
   const handleMouseDown = () => {
     setIsHidden(true);
-  }
+  };
 
   function ChatIcon({ role }: { role: 'user' | 'assistant' | 'system' }) {
     if (role === 'user') {
@@ -50,9 +50,10 @@ function ConversationPanel({
         <div
           key={conversation.id}
           className="group relative rounded-lg hover:bg-gray-200 p-2 flex flex-row space-x-3 transition-colors duration-100"
-          >
+        >
           <ChatIcon role={conversation.role} />
-          <div className="py-1 text-gray-800 markdown-content"
+          <div
+            className="py-1 text-gray-800 markdown-content"
             onMouseDown={handleMouseDown}
             onMouseUp={handleMouseUp}
             onTouchStart={handleMouseDown}
@@ -60,7 +61,11 @@ function ConversationPanel({
           >
             {marked(conversation.content ?? '')}
           </div>
-          <div className={`absolute right-2 top-2 group-hover:opacity-100 opacity-0 transition-colors duration-100 flex-row space-x-0.5 ${isHidden ? "hidden" : "flex"}`}>
+          <div
+            className={`absolute right-2 top-2 group-hover:opacity-100 opacity-0 transition-colors duration-100 flex-row space-x-0.5 ${
+              isHidden ? 'hidden' : 'flex'
+            }`}
+          >
             <TippyButton
               onClick={() => {
                 generateSpeech(conversation.content);
