fadb1af7d549e910011d251a91ccb8938b6e852a
hahahumble <hexichn@gmail.com>
2023-04-17 15:02:02 +0800

    style: format code

--- src/components/Icons/SadIcon.tsx
+++ src/components/Icons/SadIcon.tsx
@@ -19,7 +19,7 @@ const SadIcon: React.FC<SadIconProps> = ({ className, strokeWidth }) => {
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      <path d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"/>
+      <path d="M9.172 16.172a4 4 0 015.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
   );
 };
