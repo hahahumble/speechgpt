cc77bbcd3292d4f55237d492f771cde5e83971c0
hahahumble <hexichn@gmail.com>
2023-05-13 22:52:38 +0800

    chore: change padding of header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,10 @@ import SpeechGPTIcon from './Icons/SpeechGPTIcon';
 
 function Header() {
   return (
-    <div className="flex flex-col sm:pt-20 sticky pt-16">
+    <div className="flex flex-col sm:pt-16 sticky pt-16">
       <SpeechGPTIcon className="w-16 h-16 ml-2 sm:w-24 sm:h-24" />
       <div className="flex flex-row py-2 justify-between items-center w-full">
-        <div className="text-2xl font-bold text-left bg-gray-100 text-gray-800">
+        <div className="text-2xl font-bold text-left text-gray-800">
           <span className="font-bold ml-2 decoration-purple-500 animate-text text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
             SpeechGPT
           </span>
